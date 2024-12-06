d00429fda7b609981efab14893733dfd8eade278
heyheynic <nicsayhey@gmail.com>
2024-12-06 16:37:49 +0100

    currently setting up some configs and basic styling in tailwind

---
+++ src/components/ButtonRounded.jsx
@@ -0,0 +1,9 @@
+const ButtonRounded = () => {
+  return (
+    <div className="bg-foreground w-fit text-background py-2 pl-3 pr-6 rounded-r-full hover:bg-background hover:text-foreground border-foreground border-2 cursor-pointer">
+      Rounded button
+    </div>
+  );
+};
+
+export default ButtonRounded;
---
+++ src/components/ButtonSharpEdge.jsx
@@ -0,0 +1,5 @@
+const ButtonSharpEdge = () => {
+  return <div>Sharp edged button</div>;
+};
+
+export default ButtonSharpEdge;
