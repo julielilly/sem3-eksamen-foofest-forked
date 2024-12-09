1e6b6541b3bc22a4916453c74045745fba00d278
heyheynic <nicsayhey@gmail.com>
2024-12-09 13:35:23 +0100

    Trying to create a full width classname

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 import ButtonRounded from "@/components/ButtonRounded";
 import ButtonSharpEdge from "@/components/ButtonSharpEdge";
+import EXAMPLEFullComp from "@/components/EXAMPLEFullComp";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
         <ButtonRounded />
         <ButtonSharpEdge />
       </div>
+      <EXAMPLEFullComp />
     </div>
   );
 }
---
+++ src/components/EXAMPLEFullComp.jsx
@@ -0,0 +1,16 @@
+const EXAMPLEFullComp = () => {
+  return (
+    <div className=" col-full  ">
+      <img
+        src="https://images.unsplash.com/photo-1732058824460-d89cb7b4a38f?q=80&w=1472&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        alt="bild"
+        className="row-start-1 col-full"
+      />
+      <p className="col-main row-start-1 bg-red text-white self-center  h-fit">
+        This is a text to be inside main grid
+      </p>
+    </div>
+  );
+};
+
+export default EXAMPLEFullComp;
