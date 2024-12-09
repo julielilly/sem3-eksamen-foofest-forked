198bc49474f31be4279387fb6074b25b568893c1
heyheynic <nicsayhey@gmail.com>
2024-12-09 15:04:21 +0100

    Added some more styling to receipt component

--- src/components/FormReceipt.jsx
+++ src/components/FormReceipt.jsx
@@ -2,26 +2,29 @@ import ButtonRounded from "./ButtonRounded";
 
 const FormReceipt = () => {
   return (
-    <section className="border-2 border-foreground w-fit py-xs px-m ">
-      <h2 className="text-center border-b-2 border-foreground w-[50%] m-auto">
+    <section className="_receipt_component_ bg-white border-2 border-foreground pt-xs pb-m px-m min-w-fit w-auto max-w-96 ">
+      <h2 className="_receipt_title_ text-center text-step-2 font-bold border-b-2 border-foreground w-[50%] m-auto">
         Basket
       </h2>
-      <div className="_quantity_tickets border-b-2 border-foreground w-[90%]">
-        <div className="_singular_quantity_ticket_ grid grid-cols-[1fr,1fr] grid-rows-[1fr,1fr]">
-          <h3 className="row-start-1">General admission</h3>
+      <div className="_quantity_tickets border-b-2 border-foreground border:w-[90%] py-s [&>*]:py-3xs">
+        <div className="_singular_quantity_ticket_ grid grid-cols-[1fr,auto] grid-rows-[1fr,1fr]">
+          <h3 className="row-start-1 font-bold">General admission</h3>
           <p className="row-start-2">400kr</p>
-          <div className=" row-span-full self-center">- 1 + komponent</div>
+          <div className=" row-span-full self-center">- 1 +</div>
         </div>
       </div>
       <div>
-        <div className="_overview_total_price_">
-          <div>
-            <p>Name and quantity of tickets</p>
-            <p>price of tickets</p>
+        <div className="_overview_total_price_ py-s [&>*]:py-3xs">
+          <div className="flex justify-between">
+            <p>Price + quantity tickets</p>
+            <p>300kr</p>
           </div>
-          <div>booking free</div>
+          <div className="flex justify-between">
+            <p>Booking free</p>
+            <p>99kr</p>
+          </div>
         </div>
-        <div className="_tital_price_ flex justify-between">
+        <div className="_tital_price_ flex justify-between pb-m font-bold text-step-1">
           <p>Total</p>
           <p>3000kr</p>
         </div>
