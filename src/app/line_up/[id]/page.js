f5474ae8fb1f01ed8b41754382d65dc4677d9cd6
Jules KJ <juljen2730@gmail.com>
2024-12-09 12:04:48 +0100

    single view side layout

--- src/app/line_up/[id]/page.js
+++ src/app/line_up/[id]/page.js
@@ -1,4 +1,6 @@
+import { CiCalendar } from "react-icons/ci";
 import Image from "next/image";
+import ButtonSharpEdge from "@/components/ButtonSharpEdge";
 
 const page = ({ params }) => {
   // const { id } = params;
@@ -13,19 +15,49 @@ const page = ({ params }) => {
           width={200}
           className="w-[100%] h-[500px] object-cover object-top"
         ></Image>
-        <img
-          src="/wave.svg"
-          alt="Logo "
-          className="w-[100%] h-[50px] bg-rose-300 object-contain"
-        />
+
+        <Image
+          src={"/wave.png"}
+          alt={"image of something"}
+          height={300}
+          width={300}
+        ></Image>
       </header>
 
-      <h1>Artist WOOO</h1>
-
-      <div className="absolute top-1 left-0 font-bold text-text backdrop: border-l-0 text-text bg-background text-foreground w-fit py-1xs pr-s pl-l rounded-r-full  border-2 border-foreground text-nowrap">
-        <p>scene</p>
-        <h2>Midgard</h2>
-      </div>
+      <section className="mx-[0px] lg:mx-[200px]  [&>*]:my-8 mb-[100px]">
+        <h1 className="halfround-right font-bold text-title capitalize  ">
+          Artist WOOO
+        </h1>
+        <section className="grid grid-cols-1 md:grid-cols-2 gap-4">
+          <div className="font-bold text-text  bg-background text-foreground  py-1xs pr-s pl-l rounded-r-full  border-2 border-foreground text-nowrap">
+            <p>scene</p>
+            <h2>Midgard</h2>
+          </div>
+          <div className="flex justify-between items-center bg-background text-foreground  py-1xs pr-s pl-l rounded-l-full  border-2 border-foreground text-nowrap">
+            <div className="font-bold text-text">
+              <p>scene</p>
+              <h2>Midgard</h2>
+            </div>
+            <CiCalendar className="text-title" />
+          </div>
+        </section>
+        <span>PICTURE BY "some name"</span>
+        <p className="text-text font-bold">
+          artist WOOO består af medlemerne [members] . . .
+        </p>
+        <p>
+          "Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+          accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae
+          ab illo inventore veritatis et quasi architecto beatae vitae dicta
+          sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+          aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos
+          qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui
+          dolorem ipsum quia dolor 
+        </p>
+        <div className="mx-[auto] w-fit">
+          <ButtonSharpEdge></ButtonSharpEdge>
+        </div>
+      </section>
     </div>
   );
 };
