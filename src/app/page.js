06f1dc9c6f25eb6f10e4f0757dc05ee652ecb4df
heyheynic <nicsayhey@gmail.com>
2024-12-10 15:05:45 +0100

    changed fonts and folder structure

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ import FormReceipt from "@/components/FormReceipt";
 export default function Home() {
   return (
     <div>
-      <p className="font-skranji  text-title">I'm in the main.</p>
+      <p className="font-germania-one text-title">I'm in the main.</p>
       <div className="grid gap-2">
         <ButtonRounded theme="fit">Buy tickets!""!!</ButtonRounded>
         <ButtonSharpEdge theme="black">hej</ButtonSharpEdge>
