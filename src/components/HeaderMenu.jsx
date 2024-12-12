1ffb2990fa1c60041331e3dc25d820833a6c907b
Julie <julieglion@gmail.com>
2024-12-12 11:27:55 +0100

    remove unused code

--- src/components/HeaderMenu.jsx
+++ src/components/HeaderMenu.jsx
@@ -1,36 +1,20 @@
 import Link from "next/link";
 
-const HeaderMenu = ({ menuOpen, toggleMenu }) => {
+const HeaderMenu = ({ menuOpen }) => {
   return (
-    <ul
-      className={`fixed flex flex-col gap-xs top-[8rem] right-0 h-full w-2/3 max-w-xs transform transition-transform duration-300 ease-in-out ${
-        menuOpen ? "translate-x-0" : "translate-x-full"
-      }`}
-    >
+    <ul className={`fixed flex flex-col gap-xs top-[8rem] right-0 h-full w-2/3 max-w-xs transform transition-transform duration-300 ease-in-out ${menuOpen ? "translate-x-0" : "translate-x-full"}`}>
       <li>
-        <Link
-          href="/tickets"
-          className="halfround-left block w-full font-germania-one text-step-1 menu-labels"
-          onClick={toggleMenu}
-        >
+        <Link href="/tickets" className="halfround-left block w-full font-germania-one text-step-1 menu-labels">
           Tickets
         </Link>
       </li>
       <li>
-        <Link
-          href="/line_up"
-          className="halfround-left block w-full font-germania-one text-step-1 menu-labels"
-          onClick={toggleMenu}
-        >
+        <Link href="/line_up" className="halfround-left block w-full font-germania-one text-step-1 menu-labels">
           Lineup
         </Link>
       </li>
       <li>
-        <Link
-          href="/about"
-          className="halfround-left block w-full font-germania-one text-step-1 menu-labels"
-          onClick={toggleMenu}
-        >
+        <Link href="/about" className="halfround-left block w-full font-germania-one text-step-1 menu-labels">
           About
         </Link>
       </li>
