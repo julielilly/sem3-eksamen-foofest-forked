53546c17d3370bbb4c6d5952ce15e64a4e34c798
heyheynic <nicsayhey@gmail.com>
2024-12-13 12:42:49 +0100

    just changed the name of the imported components

--- src/components/tickets/FormStepOne.jsx
+++ src/components/tickets/FormStepOne.jsx
@@ -1,11 +1,11 @@
-import FormTicketBorder from "../form/FormTicketBig";
-import FormTicketSmallBorder from "../form/FormTicketSmall";
+import FormTicketBig from "../form/FormTicketBig";
+import FormTicketSmall from "../form/FormTicketSmall";
 
 const FormStepOne = () => {
   return (
     <div className="_Ticket_component_  grid mob:grid-cols-[auto,1fr]  gap-xs py-m  m-auto transition-transform ">
-      <FormTicketBorder />
-      <FormTicketSmallBorder />
+      <FormTicketBig />
+      <FormTicketSmall />
     </div>
   );
 };
