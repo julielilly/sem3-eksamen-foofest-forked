5c557216c17819cb134322f6696472347a8445ff
Jules KJ <juljen2730@gmail.com>
2024-12-13 15:09:08 +0100

    line up på forside

--- src/components/ForsideLineUp.jsx
+++ src/components/ForsideLineUp.jsx
@@ -8,30 +8,27 @@ async function ForsideLineUp() {
 
   console.log(bands[22]);
   return (
-    <ul className="flex  flex-wrap gap-2 justify-center px-[50px]">
+    <ul className="flex  flex-wrap gap-3 justify-center w">
       {bands.slice(0, 50).map((band, i) => (
-        <li className="flex items-center gap-5">
+        <li className="flex items-center gap-2 ">
           <Link
             href={`/lineup/${band.slug}`}
             className={
               i <= 15
-                ? ` text-step-3  hover:text-rose-600 hover:scale-110 transition-all hover:translate-y-[-10px] `
+                ? ` text-step-3 px-2 font-bold text-[--blue-light] hover:text-[--red] hover:scale-105 transition-all hover:translate-y-[-10px] `
                 : i <= 25
-                ? `text-step-2  hover:text-rose-600 hover:scale-110 transition-all  after:pl-4  hover:translate-y-[-10px] `
+                ? `text-step-2 px-2 font-bold text-[--green] hover:text-[--red] hover:scale-105 transition-all hover:translate-y-[-10px] `
                 : i <= 40
-                ? `text-step-1 hover:text-rose-600 hover:scale-110 transition-all  after:pl-4  hover:translate-y-[-10px] `
-                : ` text-text hover:text-rose-600 hover:scale-110 transition-all hover:translate-y-[-10px] `
+                ? `text-step-1 px-2 font-bold text-[--yellow] hover:text-[--red] hover:scale-105 transition-all hover:translate-y-[-10px] `
+                : ` text-text px-2 font-bold text-[--red] hover:text-[--red] hover:scale-105 transition-all hover:translate-y-[-10px] `
             }
             key={i}
           >
             {band.name}
           </Link>
-          <GoDotFill className="fill-red" />
+          <GoDotFill className="fill-red " />
         </li>
       ))}
-      <ButtonSharpEdge theme={"black"}>
-        <Link href={`/line_up`}>See the whole line up</Link>
-      </ButtonSharpEdge>
     </ul>
   );
 }
