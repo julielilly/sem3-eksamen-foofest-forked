ddf68ddc6c6bf9fe05b98d096c924d9ad6a69e83
Jules KJ <juljen2730@gmail.com>
2024-12-16 11:48:10 +0100

    comment

--- src/components/common/BlueSquarFull.jsx
+++ src/components/common/BlueSquarFull.jsx
@@ -1,6 +1,7 @@
 const BlueSpuarFull = ({ children, YourOwnGrid }) => {
   return (
     <section className="col-full relative bg-[--blue-light] min-h-[200px] py-10 my-10">
+      {/* write your own grid */}
       <div className={`col-main row-start-1 ${YourOwnGrid}`}>{children}</div>
     </section>
   );
