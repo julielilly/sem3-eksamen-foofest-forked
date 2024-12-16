e7df2b4df2b264c303f6c39988e6766074819fc6
Jules KJ <juljen2730@gmail.com>
2024-12-16 10:25:25 +0100

    tailwind md

--- src/components/lineup/LineUpCardCTA.jsx
+++ src/components/lineup/LineUpCardCTA.jsx
@@ -1,15 +1,8 @@
 "use client";
 import Link from "next/link";
 import Image from "next/image";
-import { useState, useEffect } from "react";
 
 const LineUpCardCTA = ({ artistName, scene, dayOfPlaying, id, src, slug }) => {
-  const [isMobile, setIsMobile] = useState(false);
-
-  useEffect(() => {
-    setIsMobile(typeof window !== "undefined" && window.innerWidth <= 600);
-  }, []);
-
   return (
     <Link key={id} href={`/lineup/${slug}`}>
       <li className="flex flex-col justify-between gap-3 h-[100%] min-h-[300px] min-w-10 max-w-[300px] relative w-fit border-2 border-foreground p-5">
@@ -28,16 +21,8 @@ const LineUpCardCTA = ({ artistName, scene, dayOfPlaying, id, src, slug }) => {
           className="border-2 border-foreground h-[100%] object-cover"
         ></Image>
 
-        <div
-          className={isMobile ? "halfround-right absolute bottom-2 left-0" : ""}
-        >
-          <h6
-            className={
-              isMobile
-                ? "font-bold text-text capitalize "
-                : "halfround-right font-bold text-text capitalize"
-            }
-          >
+        <div className="flex flex-col-reverse md:flex-col md:gap-2 halfround-right md:border-none md:bg-background  md:text-foreground md:py-0 md:pr-0  md:pl-0 md:rounded-none text-nowrap absolute bottom-2 left-0  md:relative">
+          <h6 className="font-bold text-text capitalize md:halfround-right">
             {artistName}
           </h6>
           <p className=" text-text">{dayOfPlaying}</p>
