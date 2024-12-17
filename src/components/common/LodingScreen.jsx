5cc72139fcc3b03b844c084ef3c42e9936125944
Jules KJ <juljen2730@gmail.com>
2024-12-17 19:29:23 +0100

    is loading

---
+++ src/components/common/LodingScreen.jsx
@@ -0,0 +1,43 @@
+import Image from "next/image";
+import BoatImg from "@/app/assets/boat.svg";
+import waveImage from "@/app/assets/single_wave.svg";
+
+const LoadingScreen = () => {
+  return (
+    <div className="h-[100vh] w-[100%] absolute top-0 bg-background z-10 flex flex-col items-center justify-center">
+      <Image
+        src={BoatImg}
+        alt={"image of something"}
+        height={200}
+        width={200}
+        className="origin-center  transition animate-[sail_5s_linear_infinite] "
+      ></Image>
+      <div className="flex ">
+        <Image
+          src={waveImage}
+          alt={"image of something"}
+          height={100}
+          width={100}
+          className=" transition animate-[singlewave_1s_infinite]"
+        ></Image>
+        <Image
+          src={waveImage}
+          alt={"image of something"}
+          height={100}
+          width={100}
+          className=" transition animate-[singlewave_1.1s_infinite]"
+        ></Image>
+        <Image
+          src={waveImage}
+          alt={"image of something"}
+          height={100}
+          width={100}
+          className=" transition  animate-[singlewave_1s_infinite] "
+        ></Image>
+      </div>
+      <h2 className="text-step-2 py-5">Loading Foo Fest. . .</h2>
+    </div>
+  );
+};
+
+export default LoadingScreen;
