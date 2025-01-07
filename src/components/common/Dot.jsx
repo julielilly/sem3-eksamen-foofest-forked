b01d08f853c4a0d8e5cfc27b8761fa96839ecfa6
Jules KJ <juljen2730@gmail.com>
2025-01-07 18:00:38 +0100

    ryddet op i dot classname og tilføjet rounded ril burger menu

--- src/components/common/Dot.jsx
+++ src/components/common/Dot.jsx
@@ -1,11 +1,9 @@
 const Dot = ({ item, isOpen }) => {
   return (
     <span
-      className={
-        isOpen === item
-          ? `grid place-content-center rounded-full w-8 h-8 border-2 border-foreground bg-red hover:bg-red transition-all `
-          : `grid place-content-center rounded-full w-8 h-8 border-2 border-foreground bg-white hover:bg-red transition-all `
-      }
+      className={`grid place-content-center rounded-full w-8 h-8 border-2 border-foreground hover:bg-red transition-all ${
+        isOpen === item ? "bg-red" : "bg-white "
+      }`}
     >
       <span className="rounded-full w-4 h-4 border-2 border-foreground bg-black"></span>
     </span>
