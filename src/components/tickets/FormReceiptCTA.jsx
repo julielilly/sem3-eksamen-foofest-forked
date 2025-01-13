1a094f7038b46890e502a6c924527dfb1d0c8616
Julie <julieglion@gmail.com>
2025-01-13 11:59:06 +0100

    add extra accessibility

--- src/components/tickets/FormReceiptCTA.jsx
+++ src/components/tickets/FormReceiptCTA.jsx
@@ -4,7 +4,11 @@ const FormReceiptCTA = ({ loading, isButtonDisabled, handleButtonClick, step }) 
   return (
     <div>
       {step !== 4 && (
-        <button onClick={handleButtonClick} className={`halfround-right hover:bg-background hover:text-foreground text-skranji text-left w-full ${isButtonDisabled ? "bg-[#817F77] border-[#817F77] cursor-not-allowed" : "cursor-pointer"}`}>
+        <button
+          onClick={handleButtonClick}
+          className={`halfround-right hover:bg-background hover:text-foreground text-skranji text-left w-full ${isButtonDisabled ? "bg-[#817F77] border-[#817F77] cursor-not-allowed" : "cursor-pointer"}`}
+          aria-disabled={isButtonDisabled} // set button to disabled for screen readers
+          aria-label={isButtonDisabled ? "Submit is disabled. Complete the form to enable it." : undefined}>
           {step === 1 && "Choose camping"}
           {step === 2 && (loading ? "loading..." : "Reserve tickets")}
           {step === 3 && "Go to payment"}
@@ -18,6 +22,8 @@ const FormReceiptCTA = ({ loading, isButtonDisabled, handleButtonClick, step }) 
           form="reservation-form"
           className={`halfround-right hover:bg-background hover:text-foreground text-skranji text-left w-full  ${isButtonDisabled ? "bg-[#817F77] border-[#817F77] cursor-not-allowed" : "cursor-pointer"}`}
           onClick={handleButtonClick} // Handle validation here
+          aria-disabled={isButtonDisabled} // set button to disabled for screen readers
+          aria-label={isButtonDisabled ? "Submit is disabled. Complete the form to enable it." : undefined}
         />
       )}
     </div>
